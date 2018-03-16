740a2b8c312aae490e7f81fc19f32d12acf09878
shivamswarnkar <ss8464@nyu.edu>
2018-03-15 23:35:15 -0400

    testing key bindings

--- textEditor.py
+++ textEditor.py
@@ -1,8 +1,7 @@
 import sublime
 import sublime_plugin
+import json
 
-#Save this file at 
-#/Users/UserName/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 
 #Keys for Commands
 #control+shift+s => Commit
@@ -10,14 +9,27 @@ import sublime_plugin
 #control+shift+m => Merge Branch
 #control+shift+u => Open UI
 
+def addKeyBindings():
+	keys = ['{ "keys": ["control+shift+s"], "command": "commit" },', \
+		'{ "keys": ["control+shift+b"], "command": "branch" },', \
+		'{ "keys": ["control+shift+m"], "command": "merge" },', \
+		'{ "keys": ["control+shift+u"], "command": "info" },'
+		]
 
-#Key Bindings 
-'''[
-	{ "keys": ["control+shift+s"], "command": "commit" },
-	{ "keys": ["control+shift+b"], "command": "branch" },
-	{ "keys": ["control+shift+m"], "command": "merge" },
-	{ "keys": ["control+shift+u"], "command": "info" }
-]'''
+	path = "/Users/shivamswarnkar/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap"
+	fh = open(path, 'r')
+	curr = [i.strip() for i in fh.readlines()]
+	print(curr)
+	for i in keys:
+		if(i not in curr):
+			curr.insert(len(curr)-1, i)
+	fh.close()
+
+	fh = open(path, 'w')
+	for k in curr:
+		fh.write(k+"\n")
+	fh.close()
+
 
 class CommitCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -33,3 +45,8 @@ class MergeCommand(sublime_plugin.TextCommand):
 class InfoCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.view.insert(edit, 0, "Opening GUI.....\n")
+
+
+def main():
+	
+	addKeyBindings()
