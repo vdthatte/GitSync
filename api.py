83d59a281bc713e87e827968d991e3b54cf6e75a
vdthatte <vd_thatte@yahoo.com>
2018-04-03 12:59:58 -0400

    latest

--- api.py
+++ api.py
@@ -2,4 +2,11 @@ from github import Github
 
 g = Github("c514d06de71b221a8ebb88916675b2e13a059600")
 
-print(g.get_user().avatar_url)
+repo = g.get_user().get_repo('GitSync-TestRepo')
+
+content = 'c2FzZGFzZADDDDD==\n'
+
+print(repo.create_file('/new.txt', "test commit 1", content))
+
+
+
