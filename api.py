9be4216c3ae3870ecf225a098c6b93643abbd017
vdthatte <vd_thatte@yahoo.com>
2018-04-03 13:55:24 -0400

    methods

--- api.py
+++ api.py
@@ -6,6 +6,17 @@ repo = g.get_user().get_repo('GitSync-TestRepo')
 
 content = 'c2FzZGFzZADDDDDdasdas==\n'
 
-#print(repo.create_file('/new.txt', "test commit 1", content))
+file_sha = '4407df2d7f3881db3f5db1018b2b916d55b06d14'
 
-print(repo.update_file('/hello.txt', "update", content, '4407df2d7f3881db3f5db1018b2b916d55b06d14'))
+def get_branches(repo):
+	branches = []
+	for branch in repo.get_branches():
+		branches.append(branch)
+	return branches
+
+def newFile(repo, file_name, content):
+	print(repo.create_file('/new.txt', "test commit 1", content))
+
+def updateFile(repo, file_name, content, file_sha):
+	print(repo.update_file('/' + file_name, "update", content, file_sha))
+
