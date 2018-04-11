69133694c9ab8cfe804d1c777ada15c57735bd15
vdthatte <vd_thatte@yahoo.com>
2018-04-11 17:46:30 -0400

    update new code

--- new.py
+++ new.py
@@ -1,8 +1,10 @@
-
+import sublime
 import sublime_plugin
 import json
 import sys
 import os
+import urllib
+import base64
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "github"))
 
@@ -11,9 +13,6 @@ from github import Github
 
 g = Github("c514d06de71b221a8ebb88916675b2e13a059600")
 
-repo = g.get_user().get_repo('GitSync-TestRepo')
-
-content = 'c2FzZGFzZADDDDDdasdas==\n'
 # https://api.github.com/repos/vdthatte/GitSync-TestRepo/git/trees/{latest commit sha}
 # view.run_command('commit')
 
@@ -30,7 +29,7 @@ def addKeyBindings():
 		'{ "keys": ["control+shift+u"], "command": "info" },'
 		]
 
-	path = "/Users/shivamswarnkar/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap"
+	path = "/Users/vidyadhar/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap"
 	fh = open(path, 'r')
 	curr = [i.strip() for i in fh.readlines()]
 	print(curr)
@@ -48,15 +47,19 @@ def addKeyBindings():
 class CommitCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		#self.view.insert(edit, 0, "Commited Work\n")
+		path = urllib.parse.unquote(self.view.file_name())
+		dirPath = os.path.dirname(path)
+		repoName = os.path.basename(dirPath)
+		repo = g.get_user().get_repo(repoName)
 		commit = repo.get_commits()[0]
 		tree = repo.get_git_tree(commit.sha)
+		filename = os.path.basename(path)
+		content = self.view.substr(sublime.Region(0, self.view.size()))
 
+		#print(base64.b64decode(repo.get_file_contents('/123.txt').content))
 		for element in tree.tree:
-			if(element.path == 'hello.txt'):
-				print(repo.update_file('/new.txt', "update", content, element.sha))
-
-
-
+			if(element.path == filename):
+				print(repo.update_file('/' + filename, "update", content, element.sha))
 
 class BranchCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
